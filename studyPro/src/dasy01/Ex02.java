3002daeaa2c2077959b5794a8b188683e0951ca8
sujink <sujink06@naver.com>
2025-09-01 17:12:24 +0900

    다시 복습

--- studyPro/src/dasy01/Ex02.java
+++ studyPro/src/dasy01/Ex02.java
@@ -4,6 +4,10 @@ public class Ex02 {
 
 	public static void main(String[] args) {
 		System.out.println("days01.Ex02.java 복습");
+		System.out.println("days01.Ex02.java 복습");
+		System.out.println("days01.Ex02.java 복습");
+		System.out.println("days01.Ex02.java 복습");
+		System.out.println("days01.Ex02.java 복습");
 
 	}
 
