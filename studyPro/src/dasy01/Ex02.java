28873f47fb2a7976ceffcec579e9b73780aae68c
sujink <sujink06@naver.com>
2025-09-01 17:23:07 +0900

    왜 안되는디;

---
+++ studyPro/src/dasy01/Ex02.java
@@ -0,0 +1,13 @@
+package dasy01;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		System.out.println("교육원복습");
+		System.out.println("교육원복습");
+		System.out.println("교육원복습");
+		System.out.println("교육원복습");
+
+	}
+
+}
